e28925a264220383843719e0bf5de54f3c88e758
Pedro Miranda <pedrohmirandadev@gmail.com>
2025-06-27 09:35:34 -0300

    add database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import 'dotenv/config';
+import { AppDataSource } from './database/typeorm.config';
+import { HealthModule } from './health-checker/health.module';
+import { UrlenconderModule } from './urlenconder/urlenconder.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            ...AppDataSource.options,
+            autoLoadEntities: true,
+        }),
+        UrlenconderModule,
+        HealthModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
---
+++ src/database/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+import * as path from 'path';
+import 'dotenv/config';
+
+export const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: false,
+    migrations: [path.join(__dirname, 'migrations', '*.{ts,js}')],
+});
