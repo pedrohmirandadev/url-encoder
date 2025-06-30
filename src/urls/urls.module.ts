bcc97e1e185b3b0ae5c0668796bb2ac0b720873b
Pedro Miranda <pedrohmirandadev@gmail.com>
2025-06-30 13:40:59 -0300

    rename to improve readability and ensure that user is authenticated and add increase on quantity

---
+++ src/urls/urls.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UrlService } from './urls.service';
+import { UrlController } from './urls.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Urls } from './entities/urls.entity';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Urls])],
+    controllers: [UrlController],
+    providers: [UrlService],
+    exports: [UrlService],
+})
+export class UrlModule { }
